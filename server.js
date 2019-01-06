8220e0593cd798b306e67389e87f4b22559170a4
paul sanchez <paulpsan@gmail.com>
2019-01-06 01:52:31 -0400

    config server heroku

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/dde'));
+app.use(express.static(__dirname + '/dist/minedu'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/dde/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/minedu/index.html'));
 });
 
 // default Heroku port
 app.listen(process.env.PORT || 5000);
-
